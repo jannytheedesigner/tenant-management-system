37829a007a75761db34630ffabbf1b31bf6e0738
jannytheedesigner <jannythedesigner@gmail.com>
2025-09-10 02:57:19 +0200

    Refactor layout styles for improved consistency; update logo size and navigation padding

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -3,7 +3,7 @@
 @php
     $classes =
         $active ?? false
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-white  focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
+            ? 'inline-flex items-center px-1 pt-1 border-b-4 border-[#12C6B0] text-sm font-medium leading-5 text-white  focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
             : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-white hover:text-[#12C6B0] hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
 @endphp
 
